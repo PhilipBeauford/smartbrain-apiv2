68c584af17e416f1d1bb6a645df342e3ef770d75
Philip Beauford <PhilipBeauford@github.com>
2021-09-06 05:27:17 -0700

    pg update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-transparent-36490",
     user: "postgres",
     password: "minimize1",
     database: "smartbrain",
